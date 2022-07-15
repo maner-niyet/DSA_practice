4f2946427f3a1464aef0e0dbd2a0f51d8b75d392
Maner Niyet <manerniyet15@gmail.com>
2022-07-15 00:59:16 -0400

    3sum added

---
+++ leetcode_top_interview_questions_medium/3sum.js
@@ -0,0 +1,36 @@
+/* 
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+Notice that the solution set must not contain duplicate triplets. */
+
+var threeSum = function(nums) {
+    nums.sort((a, b) => a - b);
+    
+    const result = [];
+    for (let i = 0; i < nums.length; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+        let target = nums[i];
+        let left = i + 1;
+        let right = nums.length - 1;
+        
+        while (left < right) {
+            let sum = target + nums[left] + nums[right]
+           
+            if (sum > 0) {
+                right--;
+            } else if (sum < 0) {
+                left++;
+            } else {
+                result.push([target, nums[left], nums[right]]);
+                while (nums[left] === nums[left + 1]) {
+                    left++
+                }
+                while(nums[right] === nums[right - 1]) {
+                    right--;
+                }
+                left++;
+                right--;
+            }
+        }
+    }
+    return result;
+};
