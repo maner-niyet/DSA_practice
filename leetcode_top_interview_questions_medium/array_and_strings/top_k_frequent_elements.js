922b39b0230552f9f1ec0bb17fa8c1cc0ba03f1d
Maner Niyet <manerniyet15@gmail.com>
2022-07-18 12:55:02 -0400

    top_k_frequent_elements added

--- leetcode_top_interview_questions_medium/array_and_strings/top_k_frequent_elements.js
+++ leetcode_top_interview_questions_medium/array_and_strings/top_k_frequent_elements.js
@@ -1,3 +1,4 @@
+/* Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order. */
 
 //Solution 1
 //Approach: Using hash map or JS object
