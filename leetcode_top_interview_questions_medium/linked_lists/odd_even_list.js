a7fe4aca7c878edc4e31725833c2ef0b75c7ba6d
Maner Niyet <manerniyet15@gmail.com>
2022-07-20 14:11:01 -0400

    odd_even_list added

---
+++ leetcode_top_interview_questions_medium/linked_lists/odd_even_list.js
@@ -0,0 +1,26 @@
+/* Given the head of a singly linked list, group all the nodes with odd indices together followed by the nodes with even indices, and return the reordered list.
+
+The first node is considered odd, and the second node is even, and so on.
+
+Note that the relative order inside both the even and odd groups should remain as it was in the input.
+
+You must solve the problem in O(1) extra space complexity and O(n) time complexity. */
+
+//Solution 1
+//Time: O(n) | Space(1)
+
+var oddEvenList = function(head) {
+    if (!head || !head.next) return head;
+    let odd = head;
+    let even = head.next;
+    let evenHead = head.next;
+    
+    while (even && even.next) {
+        odd.next = even.next;
+        odd = odd.next;
+        even.next = odd.next;
+        even = even.next;
+    }
+    odd.next = evenHead;
+    return head;
+}
