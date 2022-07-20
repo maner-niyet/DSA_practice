97614ee536bcdaf975b953c620e36fb876428c85
Maner Niyet <manerniyet15@gmail.com>
2022-07-20 18:55:00 -0400

    binary_tree_inorder_traversal added

---
+++ leetcode_top_interview_questions_medium/trees_and_graphs/binary_tree_inorder_traversal.js
@@ -0,0 +1,39 @@
+/* Given the root of a binary tree, return the inorder traversal of its nodes' values. */
+
+//Solution 1 - Recursive
+//Time: O(n) | Space: O(n)
+
+var inorderTraversal = function(root) {
+    let result = [];
+    function inorder(root) {
+        if (!root) return;
+        inorder(root.left);
+        result.push(root.val);
+        inorder(root.right);
+    }
+    
+    inorder(root);
+    return result;
+};
+
+//Solution 2 - Iterative with stack
+//Time: O(n) | Space: O(n)
+
+var inorderTraversal = function(root) {
+    let result = [];
+    let stack = [];
+    let current = root;
+    while (current || stack.length) {
+        while (current) {
+            stack.push(current)
+            current = current.left
+        }
+        
+        current = stack.pop()
+        result.push(current.val);
+        current = current.right;
+    }
+    return result;
+};
+
+
