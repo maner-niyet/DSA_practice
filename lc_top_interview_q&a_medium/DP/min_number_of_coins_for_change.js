b952602c6946561102a0f2f114ffc3e8a16bbca3
Maner Niyet <manerniyet15@gmail.com>
2022-08-04 12:57:32 -0400

    min_number_of_coins_for_change

---
+++ lc_top_interview_q&a_medium/DP/min_number_of_coins_for_change.js
@@ -0,0 +1,23 @@
+/* You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+
+Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+
+You may assume that you have an infinite number of each kind of coin.
+https://leetcode.com/problems/coin-change/ */
+
+//Solution 2
+//Approach: DP
+//Time: O(n * m) | Space: O(m) where n is number of coins, m is amount of money
+const coinChange = (coins, amount) => {
+    const minCoins = Array(amount + 1).fill(Infinity);
+    minCoins[0] = 0;
+    
+    for (const coin of coins) {
+        for (let a = 0; a < amount + 1; a++) {
+            if (coin <= a) {
+                minCoins[a] = Math.min(minCoins[a], 1 + minCoins[a - coin])
+            }
+        }
+    }
+    return minCoins[amount] === Infinity ? -1 : minCoins[amount];
+}
