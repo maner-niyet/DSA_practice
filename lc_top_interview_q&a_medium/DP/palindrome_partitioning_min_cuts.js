33fc5d4c4fd1c9361054ed5de8befe9a51557580
Maner Niyet <manerniyet15@gmail.com>
2022-08-09 01:07:34 -0400

    palindrome_partitioning_min_cuts sln2 added

--- lc_top_interview_q&a_medium/DP/palindrome_partitioning_min_cuts.js
+++ lc_top_interview_q&a_medium/DP/palindrome_partitioning_min_cuts.js
@@ -4,7 +4,6 @@ A palindrome is defined as a string that's written the same forward as backward.
 //Solution 1
 //Approach: DP
 //Time: O(n^3) | Space: O(n^2)
-
 const palindromePartitioningMinCuts = string => {
     const palindromes = buildPalindromes(string);
     const cuts = buildCuts(string, palindromes);
@@ -37,6 +36,51 @@ const buildPalindromes = string => {
 }
 
 const buildCuts = (string, palindromes) => {
+    const cuts = Array(string.length).fill(Infinity);
+    for (let i = 0; i < string.length; i++) {
+        if (palindromes[0][i]) {
+            cuts[i] = 0
+        } else {
+            cuts[i] = cuts[i - 1] + 1;
+            for (let j = 1; j < i; j++) {
+                if (palindromes[j][i] && cuts[j - 1] + 1 < cuts[i]) {
+                    cuts[i] = cuts[j - 1] + 1;
+                }
+            }
+        }
+    }
+    return cuts;
+}
+
+//Solution 2
+//Approach: DP
+//Time: O(n^2) | Space: O(n^2)
+const palindromePartitioningMinCuts1 = string => {
+    const palindromes = buildPalindromes(string);
+    const cuts = buildCuts(string, palindromes);
+    return cuts[cuts.length - 1];
+}
+
+const buildPalindromes1 = string => {
+  let len = string.length;
+  const palindromes = Array(len).fill(0).map(el => Array(len).fill(false));
+  for (let i = 0; i < len; i++) {
+    palindromes[i][i] = true;
+  }
+  for (let length = 2; length < len + 1; length++) {
+    for (let i = 0; i < len - length + 1; i++) {
+      j = i + length - 1;
+      if (length === 2) {
+        palindromes[i][j] = string[i] === string[j]; 
+      } else {
+        palindromes[i][j] = string[i] === string[j] && palindromes[i + 1][j - 1];
+      }
+    }
+  }
+  return palindromes;
+}
+
+const buildCuts1 = (string, palindromes) => {
     const cuts = Array(string.length).fill(Infinity);
     for (let i = 0; i < string.length; i++) {
         if (palindromes[0][i]) {
