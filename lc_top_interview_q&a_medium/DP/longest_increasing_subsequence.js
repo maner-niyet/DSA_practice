a78c1d444943e406cea0b94c0b2099dbb0ad0af5
Maner Niyet <manerniyet15@gmail.com>
2022-08-10 18:31:55 -0400

    longest_string_chain added

--- lc_top_interview_q&a_medium/DP/longest_increasing_subsequence.js
+++ lc_top_interview_q&a_medium/DP/longest_increasing_subsequence.js
@@ -1,21 +1,71 @@
-/* Given an integer array nums, return the length of the longest strictly increasing subsequence.
+/* Given a non-empty array of integers, write a function that returns the longest strictly-increasing subsequence in the array.
+A subsequence of an array is the set of numbers that aren't necessarily adjacent in the array but that are in the same order as they appear in the array. For instance, the numbers [1, 3 ,4] form a subsequence of the array [1, 2, 3, 4], and so do the numbers [2, 4]. Note that a single number in an array and the array itself are both valid subsequences of the array. 
+You can assume that there will only be one longest increasing subsequence. */
 
-A subsequence is a sequence that can be derived from an array by deleting some or no elements without changing the order of the remaining elements. For example, [3,6,2,7] is a subsequence of the array [0,3,1,6,2,2,7].
-https://leetcode.com/problems/longest-increasing-subsequence/ */
 //Solution 1
 //Approach: DP
 //Time: O(n^2) | Space: O(n)
-
-var lengthOfLIS = function(nums) {
-    const LIS = Array(nums.length).fill(1);
-    for(let i = nums.length - 1; i >= 0; i--) {
-        let currentNum = nums[i];
-        for (let j = i; j < nums.length; j++) {
-            let otherNum = nums[j];
-            if (currentNum < otherNum) {
-                LIS[i] = Math.max(1 + LIS[j], LIS[i])
-            } 
+const longestIncreasingSubsequence = array => {
+    if (array.length === 1) return array;;
+    const len = array.length,
+        LIS = Array(len).fill(1),
+        sequences = Array(len).fill(null);
+    for (let i = 1; i < len; i++) {
+      for (let j = 0; j < i; j++) {
+        if (array[i] > array[j] && LIS[i] < LIS[j] + 1) {
+            LIS[i] = LIS[j] + 1;
+            sequences[i] = j;
         }
+      }
     }
-    return Math.max(...LIS)
-};
+    const maxIdx = LIS.indexOf(Math.max(...LIS));
+    return buildSequence(array, sequences, maxIdx);
+}
+  
+const buildSequence = (array, sequences, maxIdx) => {
+    const sequence = [];
+    while (maxIdx !== null) {
+      sequence.push(array[maxIdx]);
+      maxIdx = sequences[maxIdx]
+    }
+    return sequence.reverse();
+}
+
+//Solution 2
+//Approach: DP, binary search
+//Time: O(n*logn) | Space: O(n)
+const longestIncreasingSubsequence1 = array => {
+    if (array.length === 1) return array;;
+    const len = array.length,
+          indices = Array(len + 1),
+          sequences = Array(len);
+    let maxLength = 0;
+    for (let i = 0; i < len; i++) {
+      const num = array[i];
+      const newLength = binarySearch(1, maxLength, indices, array, num);
+      sequences[i] = indices[newLength - 1];
+      indices[newLength] = i;
+      maxLength = Math.max(maxLength, newLength);
+    }
+    return buildSequence(array, sequences, indices[maxLength]);
+  }
+  
+  const binarySearch1 = (startIdx, endIdx, indices, array, num) => {
+    if (startIdx > endIdx) return startIdx;
+    const midIdx = Math.floor((startIdx + endIdx) / 2);
+    if (array[indices[midIdx]] < num) {
+      startIdx = midIdx + 1;
+    } else {
+      endIdx = midIdx - 1;
+    }
+    return binarySearch(startIdx, endIdx, indices, array, num);
+  }
+  
+  const buildSequence1 = (array, sequences, maxIdx) => {
+    const sequence = [];
+    while (maxIdx !== undefined) {
+      sequence.push(array[maxIdx]);
+      maxIdx = sequences[maxIdx];
+    }
+    return sequence.reverse();
+  }
--- lc_top_interview_q&a_medium/DP/longest_string_chain.js
+++ lc_top_interview_q&a_medium/DP/longest_string_chain.js
@@ -1,50 +1,42 @@
-/* You are given an array of words where each word consists of lowercase English letters.
-
-wordA is a predecessor of wordB if and only if we can insert exactly one letter anywhere in wordA without changing the order of the other characters to make it equal to wordB.
-
-For example, "abc" is a predecessor of "abac", while "cba" is not a predecessor of "bcad".
-A word chain is a sequence of words [word1, word2, ..., wordk] with k >= 1, where word1 is a predecessor of word2, word2 is a predecessor of word3, and so on. A single word is trivially a word chain with k == 1.
-
-Return the length of the longest possible word chain with words chosen from the given list of words.
-
-https://leetcode.com/problems/longest-string-chain/ */
-
-//Solution 1
-//Approach: Hashmap, Recursion, Memoization
-//Time: O(n*c^2) | Space: O(n) 
-//where n is number of words, c is number of chars in word
-//https://www.youtube.com/watch?v=zqe_zIkyVGQ&ab_channel=Errichto
-const longestStringChain = words => {
-    const map = mapWords(words);
-    const edges = words.map(el => []);
-    const memo = words.map(el => 0);
-    let n = words.length;
-    for (let i = 0; i < n; i++) {
-        let word = words[i];
-        for (let j = 0; j < word.length; j++) {
-            let match = word.substring(0, j) + word.substring(j + 1);
-            if (!map.hasOwnProperty(match)) continue;
-            edges[map[match]].push(j);
+const longestStringChain = strings => {
+    strings.sort((a, b) => a.length - b.length); 
+    const map = buildMap(strings);
+    const len = strings.length;
+    for (let i = 1; i < len; i++) {
+      let word = strings[i];
+      for (let j = 0; j < word.length; j++) {
+        let newWord = word.substring(0, j) + word.substring(j + 1);
+        if (!(newWord in map)) continue;
+        if (map[word][1] < map[newWord][1] + 1) {
+          map[word][1] = map[newWord][1] + 1;
+          map[word][0] = newWord;
         }
-    }
-    let answer = 0;
-    for (let i = 0; i < n; i++) {
-        answer = Math.max(answer, getLongest(i, edges, memo))
-    }
+      }
+    }  
+    return buildChain(map);
 }
-
-const getLongest = (i, edges, memo) => {
-    if (memo[i] > 0) return memo[i];
-    for (const neighbor of edges) {
-        memo[i] = math.max(memo[i], getLongest(neighbor, edges, memo) + 1);
+  
+const buildMap = strings => {
+    const map = {};
+    for (const string of strings) {
+      map[string] = ["", 1]
     }
-    return memo[i];
-}
-
-const mapWords = words => {
-    const map = words.reduce((obj, word, index) => {
-        obj[word] = index;
-        return obj;
-    }, {})
     return map;
-}
+ }
+  
+ const buildChain = (map) => {
+    let maxLength = -Infinity;
+    let beginningOfChain = "";
+    const chain = [];
+    for (const key in map) {
+      if (map[key][1] > maxLength) {
+        maxLength = map[key][1];
+        beginningOfChain = key;
+      }
+    }
+    while (beginningOfChain !== "") {
+      chain.push(beginningOfChain);
+      beginningOfChain = map[beginningOfChain][0];
+    }
+    return chain.length === 1 ? [] : chain;
+ }
